30c1a82065462894d1b097f11921713c4050487b
Yusuf <youssoufcameroon@gmail.com>
2021-12-04 21:11:56 +0200

    Adding new files and configurations

---
+++ opencart_cucumber/features/LoginDDT.feature
@@ -0,0 +1,45 @@
+Feature: LoginDDT
+
+  Background: Common pre-steps for all Login Scenarios
+    Given User Launch browser
+    And opens URL "https://demo.opencart.com/"
+    When User navigate to MyAccount menu
+    And click on Login
+
+  Scenario Outline: Login Data Driven
+#    Given User Launch browser
+#    And opens URL "https://demo.opencart.com/"
+#    When User navigate to MyAccount menu
+#    And click on Login
+    And User enters Email as "<email>" and Password as "<password>"
+    And Click on Login button
+    Then User navigates to MyAccount Page
+
+    Examples:
+      | email                     | password |
+      | pavanoltraining@gmail.com | test123  |
+      | pavanol@gmail.com         | test123  |
+
+  Scenario Outline: Login Data Driven2
+#    Given User Launch browser
+#    And opens URL "https://demo.opencart.com/"
+#    When User navigate to MyAccount menu
+#    And click on Login
+    Then check User navigates to MyAccount Page by passing Email and Password with excel row "<row_index>"
+
+    Examples:
+      |row_index|
+      |1|
+      |2|
+      |3|
+
+#    the below secanrio both have common steps then we can delete those steps and add to bacground above
+#  Scenario Outline: Login Data Driven
+#    Given User Launch browser.Feature:
+#    backgound will execute before executing any scenario in the feature you can not execute in the middle
+#
+#
+#
+
+
+
--- opencart_cucumber/src/test/java/testRunner/TestRun.java
+++ opencart_cucumber/src/test/java/testRunner/TestRun.java
@@ -8,11 +8,25 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions
         (
-                features= {".//Features/"}, //specify the feature you want ot run
-                glue="stepDefinitions", //specify the package name of the steps definition location
+                /*features= {".//Features/"}, //specify the feature you want ot run
+                glue="stepDefinitions",//specify the package name of the steps definition location
                 //execution always start with runner class no need to execute steps or login features
                 //if they are many feature. You can put .//Features/Login.feature
-                monochrome=true // with this one added you will not get unncessary character in output 
+                */
+                /*features= {".//Features/LoginDDT.feature"}, //this will exe
+                glue="stepDefinitions",
+                dryRun=false  if yo make it false then it will execute normally by taking the data
+                It is specially used in the stage when you will have to see if there are any compilation errors
+                or if any steps is missing.
+
+                feature file can be executed parallely but scenario can not be
+
+                After adding scenario in accoutn registration feature page the step that are not implemented will be
+                highlited and if you run the testrun it will provide tose steps and you just need to copy them
+                */
+                features= {".//Features/Login.feature",".//Features/AccountRegistration.feature"}, //this will exe
+                glue="stepDefinitions",
+                dryRun=false
 
         )
 
